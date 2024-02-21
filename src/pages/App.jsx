35e02a3471d3df98d9872a3654678cf69bab9e43
Bryan De Jesús Rosa Tavarez <20212239@itla.edu.do>
2024-02-21 02:15:59 -0400

    config project

---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'flowbite-react';
+import { Avatar } from 'flowbite-react';
+
+export default function App() {
+  return  <div className="flex flex-wrap gap-2">
+  <Avatar img="https://www.flowbite-react.com/images/people/profile-picture-5.jpg" alt="avatar of Jese" rounded />
+  <Avatar img="https://www.flowbite-react.com/images/people/profile-picture-5.jpg" />
+</div>;
+
+  
+}
+
+
