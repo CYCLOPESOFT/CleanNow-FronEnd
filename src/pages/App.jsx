7f7628078f06bd3ae9669aa3ae8b3d853009c525
Bryan De Jesús Rosa Tavarez <20212239@itla.edu.do>
2024-02-29 13:52:45 -0400

    Delete unused files and update dependencies and update desing app(login)

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import Login from './Auth/Login';
-export default function App() {
-  return (
-  
-<div className="flex flex-wrap gap-2">
-    <Login />
-</div>);
-  
-}
+import React from "react";
+import RoutesApp from "../routes/routes";
+import Landing from "./LandingPage/landing";
+("./LandingPage/landing");
 
+const App = () => {
+  const [isAnimating, setAnimating] = useState(false);
 
+  if (isAnimating) {
+    return (
+      <div className="app">
+        <RoutesApp />
+      </div>
+    );
+  } else {
+    return (
+      <div className="app">
+        <Landing setAnimating={setAnimating} />
+      </div>
+    );
+  }
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
+  darkMode: "class", // or 'media' or 'class
   theme: {
     extend: {
-      colors:{
-        blackLogin: '#111828',
-        blueLogin: '#1C65F2',
-        grayInput: '#323A4A'
-      }
+      colors: {
+        blackLogin: "#181f29",
+        blueLogin: "#1C65F2",
+        grayInput: "#323A4A",
+        appColor: "#7100df",
+        hoverAppColor: "#9264ff",
+        activeAppColor: "#6645b2",
+      },
     },
-    
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), require("tailwindcss-animated")],
 };
