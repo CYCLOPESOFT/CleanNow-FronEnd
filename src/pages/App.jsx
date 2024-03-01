8dda9754fbe3632258af8b2c79cb70808344fbf2
JuanAddiel <addieljaquez@gmail.com>
2024-03-01 02:14:42 -0400

    ResolveChange

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,39 +1,15 @@
-<<<<<<< HEAD
 import React from "react";
 import RoutesApp from "../routes/routes";
-import Landing from "./LandingPage/landing";
-("./LandingPage/landing");
-=======
-import React from 'react';
-import Login from './Auth/Login';
 
-export default function App() {
-  return (
-    <div className="flex flex-wrap gap-2">
-      {<Login />}
-
-    </div>
-
-  )
-}
->>>>>>> main
 
 const App = () => {
-  const [isAnimating, setAnimating] = useState(false);
 
-  if (isAnimating) {
-    return (
-      <div className="app">
-        <RoutesApp />
-      </div>
-    );
-  } else {
-    return (
-      <div className="app">
-        <Landing setAnimating={setAnimating} />
-      </div>
-    );
-  }
+
+  return (
+    <div className="app">
+      <RoutesApp />
+    </div>
+  );
 };
 
 export default App;
