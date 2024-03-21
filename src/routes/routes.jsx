c09d6b44564b2987d664d9d1d4799798f9ef9135
JuanAddiel <addieljaquez@gmail.com>
2024-03-20 20:21:23 -0400

    Add Slices

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -9,12 +9,13 @@ import RecoveryPassword from "../pages/Auth/RecoveryPassword";
 import SendVerification from "../pages/Auth/SendVerification";
 import VerificationNumber from "../pages/Auth/VerificationNumber";
 import UserData from "../pages/Auth/UserData";
+import HomePage from "../pages/Home/HomePage";
 
 const RoutesApp = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Login />} />
-      <Route path="about" element={<About />} />
+      <Route path="about" element={<HomePage />} />
       <Route path="services" element={<Services />} />
       <Route path="prices" element={<></>} />
       <Route path="register" element={<UserData />} />
