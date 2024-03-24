c9c3356e807fb24a190f492c9c8e661c37655beb
Adalberto Vargas <20211911@itla.edu.do>
2024-03-23 20:09:48 -0400

    nothing

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,11 +4,12 @@ import HomePage from "./Home/HomePage";
 import Solicitudes from "./Home/Solicitudes";
 import ServicesPage from "./Home/ServicesPage";
 import PerfilPage from "./Home/PerfilPage";
+import ListaContacto from "./Chat/ListaContacto";
 
 const App = () => {
   return (
     <div className="app">
-      <PerfilPage />
+      <ListaContacto />
     </div>
   );
 };
